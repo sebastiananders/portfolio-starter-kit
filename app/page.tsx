500a2206376d3f505c1e0d3e423c78ad4d24d4c1
sebastiananders <sebastian.anders@aighty-twenty.com>
2025-01-22 13:47:29 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h1 className="mb-8 text-2xl font-semibold tracking-tight">
         In the loop: AI & UX
       </h1>
       <p className="mb-4">
-        {`Hi, my name is Sebastian and I am a design lead for an AI start up. 
+        {`Hi, my name is Sebastian and I am the design lead for an AI start up. 
         Here I reflect on my daily work and think out loud about design and AI in general`}
       </p>
       <div className="my-8">
