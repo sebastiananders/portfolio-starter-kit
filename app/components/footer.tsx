9e5469528771b4b6ef6c9fa7e8ea6fbd43a41571
sebastiananders <sebastian.anders@aighty-twenty.com>
2025-01-26 17:02:01 +0100

    changed footer settings

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -35,10 +35,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/vercel/next.js"
+            href="https://www.linkedin.com/in/hey-sebastian/"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">github</p>
+            <p className="ml-2 h-7">linkedin</p>
           </a>
         </li>
         <li>
@@ -49,7 +49,7 @@ export default function Footer() {
             href="https://vercel.com/templates/next.js/portfolio-starter-kit"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
+            <p className="ml-2 h-7">Vercel \o/</p>
           </a>
         </li>
       </ul>
