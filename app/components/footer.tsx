e1f26039bfd81d8627d06f191bfe1f8b260f9f69
sebastiananders <sebastian.anders@aighty-twenty.com>
2025-01-26 17:05:43 +0100

    footer links changed

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
   return (
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
+        {/* <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
@@ -29,7 +29,7 @@ export default function Footer() {
             <ArrowIcon />
             <p className="ml-2 h-7">rss</p>
           </a>
-        </li>
+        </li> */}
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
@@ -46,10 +46,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://sebastiananders.framer.website/"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">Vercel \o/</p>
+            <p className="ml-2 h-7">work</p>
           </a>
         </li>
       </ul>
