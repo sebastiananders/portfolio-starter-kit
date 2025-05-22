978d203423a64beec86a7041c4b3eb0a50150f50
sebastiananders <hi@sebastian-anders.com>
2025-05-22 09:40:02 +0200

    Chore: update blog post layout and dependencies

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -21,7 +21,7 @@ export function BlogPosts() {
             className="flex flex-col space-y-1 mb-4"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+            <div className="w-full flex items-baseline space-x-4">
               <p className="text-neutral-600 dark:text-neutral-400 whitespace-nowrap tabular-nums">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
